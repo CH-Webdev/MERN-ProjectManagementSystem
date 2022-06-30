2eadcddc1d4d16eadedd3cf85a03662a7298883f
Chad Heidesch <chadheidesch@gmail.com>
2022-06-30 15:58:14 -0500

    logo and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 
 function App() {
   return (
+    <>
+    <Header />
     <div className="container">
       <h1>Hello</h1>
     </div>
+    </>
   );
 }
 
